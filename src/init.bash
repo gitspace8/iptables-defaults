59148bd565ccd9d7215b191fb344a4efe96d8605
Dmitriy Dupliy <dupliy.dv@gmail.com>
2023-06-26 16:55:54 +0300

    add simple nat add simple commands

--- src/init.bash
+++ src/init.bash
@@ -3,7 +3,7 @@
 
 
 # init iptables-persistent
-sudo apt install wget vim mc curl htop -y
+sudo apt install wget vim mc curl htop git -y
 # init iptables-persistent
 sudo apt install iptables-persistent -y
 # set timezone
@@ -14,3 +14,7 @@ sudo cat /proc/sys/net/ipv4/ip_forward
 # sudo nano /etc/sysctl.conf
 # net.ipv4.ip_forward = 1
 # sudo /sbin/sysctl -p
+
+# locale
+sudo dpkg-reconfigure locales
+sudo localectl set-locale LANG=ru_RU.utf8
